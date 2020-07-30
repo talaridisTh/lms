7c97d355934fed1d1703eba264e75ffc439d7aa8
Evangelos Bitsilis <e.bitsilis@gmail.com>
2020-07-30 17:02:02 +0300

    model Bundle -a, migration - factory

---
+++ database/factories/BundleFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Bundle;
+use Faker\Generator as Faker;
+use Illuminate\Support\Carbon;
+
+$factory->define(Bundle::class, function (Faker $faker) {
+
+	static $counter = 1;
+
+	$year = rand(2015, 2020);
+	$month = rand(1, 12);
+	$day = rand(1, 31);
+	$hours = rand(0, 23);
+	$mins = rand(0, 59);
+	$secs = rand(0, 59);
+
+	$date = Carbon::create($year, $month, $day, $hours, $mins, $secs);
+
+    return [
+        'name' => "Name of Bundle ".$counter,
+		'description' => "Description of Bundle ".$counter++,
+		'cover' => $faker->md5 .".jpg",
+		'slug' => $faker->slug,
+		'active' => $faker->numberBetween( 0, 1 ),
+		'created_at' => $date->format('Y-m-d H:i:s'),
+		'updated_at' => $date->addWeeks(rand(1, 12))->subSeconds(rand(36000, 136000))->format('Y-m-d H:i:s')
+    ];
+});
---
+++ database/seeds/BundleSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class BundleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\Bundle::class, 3)->create();
+    }
+}
