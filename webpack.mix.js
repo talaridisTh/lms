56a4e5361b067ff67030a8b5b2948949e3dd7abc
thanos thanasis <you@example.com>
2020-07-31 17:26:24 +0300

    instal sourceMaps & disableSuccessNotification  (webpack)

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,4 +14,7 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .browserSync("http://127.0.0.1:8000/")
+    .sourceMaps()
+    .disableSuccessNotifications()
     .version();
+
