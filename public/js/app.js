36632a39f6886620bc34c579efccf2305fc3af01
Evangelos Bitsilis <e.bitsilis@gmail.com>
2020-08-01 22:03:13 +0300

    unsaved mix-manifest

--- public/js/app.js
+++ public/js/app.js
@@ -515,8 +515,8 @@ eval("// removed by extract-text-webpack-plugin//# sourceURL=[module]\n//# sourc
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\laragon\www\lms-idrogios\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\laragon\www\lms-idrogios\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\Coding\Real Projects\Demo LMS\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\Coding\Real Projects\Demo LMS\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
