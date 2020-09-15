e3476a11a7ff0648959f63c90c68f66c92445dbb
Evangelos Bitsilis <e.bitsilis@gmail.com>
2020-09-15 23:51:00 +0300

    added thumbnail images

--- app/Http/Controllers/Ajax/MediaController.php
+++ app/Http/Controllers/Ajax/MediaController.php
@@ -102,11 +102,6 @@ class MediaController extends Controller
 
 	public function editorImages ( Request $request ) {
 
-
-
-    	 dd($request->all());
-
-
 		//! model Sended with request	etc. App\Course - App\Bundle
 		//! an telika den xrisimopoih8ei o pivot na afere8i kai apo
 		//! ta js arxeia
