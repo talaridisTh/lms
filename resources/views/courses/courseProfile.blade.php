fe90372b0bdc73648fb8ae4c6ee39b31171f6d5b
thanos thanasis <you@example.com>
2020-09-23 19:30:40 +0300

    make small insructor container

--- resources/views/courses/courseProfile.blade.php
+++ resources/views/courses/courseProfile.blade.php
@@ -146,46 +146,46 @@
                 </div>
             </div>
         </div>
-        <div class="container" style="max-width: 1250px">
-            <div class="row background-material">
-                <div class="col-md-12">
-                    <div class="row justify-content-between px-3 py-2">
-                        <div class="col-md-6"><span class="font-weight-bold text-black">Εισηγητής </span>|
-                            {{$course->curator->first_name}}
-                        </div>
-                        <div class="col-md-6 text-right">
-                            <i class="{{$course->curator->facebook_link?"mdi cursor-pointer h3 mdi-facebook":""}}"></i>
-                            <i class="{{$course->curator->instagram_link?"mdi cursor-pointer h3 mdi-instagram":""}}"></i>
-                            <i class="{{$course->curator->youtube_link?"mdi cursor-pointer h3 mdi-youtube":""}}"></i>
-                            <i class="{{$course->curator->linkedin_link?"mdi cursor-pointer h3 mdi-linkedin":""}}"></i>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-12 ">
-                    <div class="row mb-1">
-                        <div class="col-md-2 pt-2 text-center">
-                            <img height="80" width="80" class="rounded-circle"
-                                 src="{{url($course->curator->cover)}}"
-                                 alt=""></div>
-                        <div class="col-md-10 text-black d-flex flex-column justify-content-center ">
-                            <div class="row">
-                                <div class="col-md-9">
-                                    <h4> </h4>
-                                    <p> {!! $course->curator->profil !!}
-                                    </p>
-                                </div>
-                                <div class="col-md-3"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
 
-        </div>
         <div class="container-fluid my-3" style="max-width: 1250px">
             <div class="row ">
 
                 <div class="col-md-8 {{empty($course->description)?"d-none":""}}">
+                    <div class="row background-material">
+                        <div class="col-md-12">
+                            <div class="row justify-content-between py-3 " style="padding-left:4.5rem!important ">
+                                <div class="col-md-6"><span class="font-weight-bold text-black">Εισηγητής </span>|
+                                    {{$course->curator->first_name}}
+                                </div>
+                                <div class="col-md-6 text-right">
+                                    <i class="{{$course->curator->facebook_link?"mdi cursor-pointer h3 mdi-facebook":""}}"></i>
+                                    <i class="{{$course->curator->instagram_link?"mdi cursor-pointer h3 mdi-instagram":""}}"></i>
+                                    <i class="{{$course->curator->youtube_link?"mdi cursor-pointer h3 mdi-youtube":""}}"></i>
+                                    <i class="{{$course->curator->linkedin_link?"mdi cursor-pointer h3 mdi-linkedin":""}}"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-12 pl-3">
+                            <div class="row mb-1 pl-4">
+                                <div class="col-md-2 pt-2 text-center">
+                                    <img height="80" width="80" class="img-fluid img-thumbnail rounded-circle"
+                                         src="  {{$course->curator->cover}}"
+                                         alt=""></div>
+                                <div class="col-md-10 text-black d-flex flex-column justify-content-center ">
+                                    <div class="row">
+                                        <div class="col-md-9">
+                                            <h4> </h4>
+                                            <p> {!! $course->curator->profil !!}
+                                            </p>
+                                        </div>
+                                        <div class="col-md-3"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
                     {!! $course->description !!}
                 </div>
                 <div class=" {{!empty($course->description)?"col-md-4":"offset-2 col-md-8 offset-2  text-left" }}">
