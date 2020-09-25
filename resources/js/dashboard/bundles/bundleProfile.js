1a661dc7b9c87185cd40310f0602e93c2ad96f19
Evangelos Bitsilis <e.bitsilis@gmail.com>
2020-09-25 21:52:58 +0300

    bundles article editor update

--- resources/js/dashboard/bundles/bundleProfile.js
+++ resources/js/dashboard/bundles/bundleProfile.js
@@ -732,7 +732,8 @@ ArticleEditor('#description', {
 	css: "/css/",
 	custom: {
 		css: [
-			"/css/bootstrap.min.css"
+			"/css/bootstrap.min.css",
+			"/css/customArticleStyle.css",
 		]
 	},
 	plugins: ['mediaLibrary'],
