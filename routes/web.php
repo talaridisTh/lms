718765de0e2e9cefb8e4b2e0d7720689050b2a93
thanos thanasis <you@example.com>
2020-10-01 00:17:59 +0300

    404 erro fix

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\URL;
-use Spatie\Activitylog\Models\Activity;
-use Spatie\UrlSigner\MD5UrlSigner;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -18,10 +16,14 @@ use Spatie\UrlSigner\MD5UrlSigner;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get("/expot/users", "ExportController@usersAll")->name("export.usersAll");
-Route::get('/', function () {
-    return view('welcome');
+Auth::routes();
+//!########################################################
+//! 404
+Route::fallback(function () {
+    return redirect(route("home"));
 });
+//!########################################################
+//! clear catch
 Route::get('/clear', function () {
     Artisan::call('cache:clear');
 
@@ -45,7 +47,6 @@ Route::group(['middleware' => ['auth', "role:admin"]], function () {
     //! Material Routes
     Route::get('/dashboard/materials', 'MaterialController@index')->name('material.index');
     Route::get('/dashboard/material/{material?}', 'MaterialController@show')->name('material.show');
-    // Route::get('/dashboard/materials/create', 'MaterialController@create')->name('material.create');
     Route::post('/dashboard/materials/store', 'MaterialController@store')->name('material.store');
     Route::patch('/dashboard/materials/update/{material:slug}', 'MaterialController@update')->name('material.update');
     Route::delete('/dashboard/materials/delete/{material}', 'MaterialController@destroy')->name('material.destroy');
@@ -70,7 +71,6 @@ Route::group(['middleware' => ['auth', "role:admin"]], function () {
     //! Topic Routes
     Route::get('/dashboard/topics', 'TopicController@index')->name('topic.index');
     Route::post('/dashboard/topics/store', 'TopicController@store')->name('topic.store');
-});
 //!======================================================
 //! 			End Dashboard Routes					|
 //!======================================================
@@ -78,114 +78,112 @@ Route::group(['middleware' => ['auth', "role:admin"]], function () {
 //! AJAX routes
 //!########################################################
 //! Dashboard Ajax Users Datatables
-Route::post('users/view-users', 'Ajax\UserController@index')->name("index.datatable");
-Route::post('users/view-user', 'Ajax\UserController@show')->name("show.datatable");
-Route::post('/user/add-course-modal', 'Ajax\UserController@addCourseModal')->name("courseModal.datatable");
-Route::post('/user/courses-inside-users', 'Ajax\UserController@coursesInsideUsers')->name("coursesInsideUsers.datatable");
+    Route::post('users/view-users', 'Ajax\UserController@index')->name("index.datatable");
+    Route::post('users/view-user', 'Ajax\UserController@show')->name("show.datatable");
+    Route::post('/user/add-course-modal', 'Ajax\UserController@addCourseModal')->name("courseModal.datatable");
+    Route::post('/user/courses-inside-users', 'Ajax\UserController@coursesInsideUsers')->name("coursesInsideUsers.datatable");
 //! Dashboard Ajax Users CRUD
-Route::patch('/user/add-course', 'Ajax\UserController@addCourses')->name("addcourses.datatable");
-Route::patch('/user/changeStatus', 'Ajax\UserController@changeStatus')->name("changeStatus.datatable");;
-Route::patch('/user/multiple/changeStatus', 'Ajax\UserController@changeStatusMultiple')->name("changeStatusMultiple.datatable");;
-Route::patch('/user/multiple/add-course', 'Ajax\UserController@addCoursesMultipleUsers')->name("addCoursesMultipleUsers.datatable");
-Route::patch('/user/multiple/add-user', 'Ajax\UserController@AddMultipleUserCourse')->name("AddMultipleUserCourse.datatable");
-Route::delete('/user/delete', 'Ajax\UserController@destroy')->name("destroy.datatable");
-Route::delete('/user/multiple/courses/delete', 'Ajax\UserController@destroyMultipleCourses')->name("destroyMultipleCourses.datatable");
-Route::delete('/user/multiple/users/delete', 'Ajax\UserController@destroyMultipleUsers')->name("destroyMultipleUsers.datatable");
-Route::post("/user/show-password", "Ajax\UserController@showPassword")->name("showPassword");
-Route::post('/users/avatar/upload', 'Ajax\UserController@avatarUpload')->name('user.avatar.upload');
+    Route::patch('/user/add-course', 'Ajax\UserController@addCourses')->name("addcourses.datatable");
+    Route::patch('/user/changeStatus', 'Ajax\UserController@changeStatus')->name("changeStatus.datatable");;
+    Route::patch('/user/multiple/changeStatus', 'Ajax\UserController@changeStatusMultiple')->name("changeStatusMultiple.datatable");;
+    Route::patch('/user/multiple/add-course', 'Ajax\UserController@addCoursesMultipleUsers')->name("addCoursesMultipleUsers.datatable");
+    Route::patch('/user/multiple/add-user', 'Ajax\UserController@AddMultipleUserCourse')->name("AddMultipleUserCourse.datatable");
+    Route::delete('/user/delete', 'Ajax\UserController@destroy')->name("destroy.datatable");
+    Route::delete('/user/multiple/courses/delete', 'Ajax\UserController@destroyMultipleCourses')->name("destroyMultipleCourses.datatable");
+    Route::delete('/user/multiple/users/delete', 'Ajax\UserController@destroyMultipleUsers')->name("destroyMultipleUsers.datatable");
+    Route::post("/user/show-password", "Ajax\UserController@showPassword")->name("showPassword");
+    Route::post('/users/avatar/upload', 'Ajax\UserController@avatarUpload')->name('user.avatar.upload');
 //! Dashboard Ajax Media
-Route::get('/users/media/{mediaItem}/{size?}', 'MediaController@showMedia')->name('api.media.show');
-Route::post("file-details-store", "Ajax\MediaController@store")->name("file.details.store");
-Route::patch("media/remove-cover", "Ajax\MediaController@removeCover");
+    Route::get('/users/media/{mediaItem}/{size?}', 'MediaController@showMedia')->name('api.media.show');
+    Route::post("file-details-store", "Ajax\MediaController@store")->name("file.details.store");
+    Route::patch("media/remove-cover", "Ajax\MediaController@removeCover");
 //! Dashboard Ajax Courses Datatables
-Route::post('courses/courses-datatable', 'Ajax\CourseController@index');
-Route::post('courses/course-materials-datatable', 'Ajax\CourseController@courseMaterials');
-Route::post('courses/not-incourse-materials-datatable', 'Ajax\CourseController@remainingMaterials');
-Route::post('courses/course-users-datatable', 'Ajax\CourseController@courseUsers');
-Route::post('courses/add-course-students-datatable', 'Ajax\CourseController@addCourseStudents');
+    Route::post('courses/courses-datatable', 'Ajax\CourseController@index');
+    Route::post('courses/course-materials-datatable', 'Ajax\CourseController@courseMaterials');
+    Route::post('courses/not-incourse-materials-datatable', 'Ajax\CourseController@remainingMaterials');
+    Route::post('courses/course-users-datatable', 'Ajax\CourseController@courseUsers');
+    Route::post('courses/add-course-students-datatable', 'Ajax\CourseController@addCourseStudents');
 //! Dashboard Ajax Courses CRUD
-Route::delete('courses/destroy/{ids}', 'Ajax\CourseController@destroy');
-Route::patch('courses/status', 'Ajax\CourseController@toggleStatus');
-Route::patch('courses/priority', 'Ajax\CourseController@changePriority');
-Route::patch('courses/toggle-materials', 'Ajax\CourseController@toggleCourseMaterials');
-Route::post('courses/add-materials', 'Ajax\CourseController@addMaterials');
-Route::patch('courses/remove-materials', 'Ajax\CourseController@removeMaterials');
-Route::patch('courses/add-students', 'Ajax\CourseController@addStudents');
-Route::patch('courses/remove-students', 'Ajax\CourseController@removeStudents');
+    Route::delete('courses/destroy/{ids}', 'Ajax\CourseController@destroy');
+    Route::patch('courses/status', 'Ajax\CourseController@toggleStatus');
+    Route::patch('courses/priority', 'Ajax\CourseController@changePriority');
+    Route::patch('courses/toggle-materials', 'Ajax\CourseController@toggleCourseMaterials');
+    Route::post('courses/add-materials', 'Ajax\CourseController@addMaterials');
+    Route::patch('courses/remove-materials', 'Ajax\CourseController@removeMaterials');
+    Route::patch('courses/add-students', 'Ajax\CourseController@addStudents');
+    Route::patch('courses/remove-students', 'Ajax\CourseController@removeStudents');
 //! Dashboard Ajax Bundles Datatables
-Route::post('bundles/bundles-datatable', 'Ajax\BundleController@index');
-Route::post('bundles/bundle-courses-datatable', 'Ajax\BundleController@show');
-Route::post('bundles/bundle-users-datatable', 'Ajax\BundleController@bundleUsers');
-Route::post('bundles/remaining-courses-datatable', 'Ajax\BundleController@remainingCourses');
-Route::post('bundles/remaining-users-datatable', 'Ajax\BundleController@remainingUsers');
+    Route::post('bundles/bundles-datatable', 'Ajax\BundleController@index');
+    Route::post('bundles/bundle-courses-datatable', 'Ajax\BundleController@show');
+    Route::post('bundles/bundle-users-datatable', 'Ajax\BundleController@bundleUsers');
+    Route::post('bundles/remaining-courses-datatable', 'Ajax\BundleController@remainingCourses');
+    Route::post('bundles/remaining-users-datatable', 'Ajax\BundleController@remainingUsers');
 //! Dashboard Ajax Bundles CRUD
-Route::delete('bundles/destroy/{ids}', 'Ajax\BundleController@destroy');
-Route::patch('bundles/status', 'Ajax\BundleController@toggleStatus');
-Route::patch('bundles/add-courses', 'Ajax\BundleController@addCourses');
-Route::patch('bundles/remove-courses', 'Ajax\BundleController@removeCourses');
-Route::post('bundles/remove-users', 'Ajax\BundleController@removeUsers');
-Route::post('bundles/add-users', 'Ajax\BundleController@addUsers');
+    Route::delete('bundles/destroy/{ids}', 'Ajax\BundleController@destroy');
+    Route::patch('bundles/status', 'Ajax\BundleController@toggleStatus');
+    Route::patch('bundles/add-courses', 'Ajax\BundleController@addCourses');
+    Route::patch('bundles/remove-courses', 'Ajax\BundleController@removeCourses');
+    Route::post('bundles/remove-users', 'Ajax\BundleController@removeUsers');
+    Route::post('bundles/add-users', 'Ajax\BundleController@addUsers');
 //! Dashboard Ajax Materials Datatables
-Route::post('materials/materials-datatable', 'Ajax\MaterialController@index');
-Route::post('materials/materials-course-datatable', 'Ajax\MaterialController@indexCourse')->name("material-courses-datatable");
-Route::post('materials/add-course-inside-material', 'Ajax\MaterialController@addCourseMaterial')->name("add-course-material-datatable");
+    Route::post('materials/materials-datatable', 'Ajax\MaterialController@index');
+    Route::post('materials/materials-course-datatable', 'Ajax\MaterialController@indexCourse')->name("material-courses-datatable");
+    Route::post('materials/add-course-inside-material', 'Ajax\MaterialController@addCourseMaterial')->name("add-course-material-datatable");
 //! Dashboard Ajax Bundles CRUD
-Route::post('materials/material-types', 'Ajax\MaterialController@materialTypes');
-Route::post('materials/add-additionnal-content', 'Ajax\MaterialController@addContent');
-Route::delete('/materials/multiple/delete', 'Ajax\MaterialController@destroyMultipleMaterials')->name("destroyMultipleMaterials.datatable");
-Route::patch('/materials/multiple/add-material', 'Ajax\MaterialController@addMaterialMultiple')->name("addMaterialMultiple.datatable");
-Route::patch('/materials/multiple/changeStatus', 'Ajax\MaterialController@changeStatusMultiple')->name("changeStatusMultipleMaterial.datatable");;
-Route::patch('materials/toggle-active/{material}', 'Ajax\MaterialController@toggleActive');
+    Route::post('materials/material-types', 'Ajax\MaterialController@materialTypes');
+    Route::post('materials/add-additionnal-content', 'Ajax\MaterialController@addContent');
+    Route::delete('/materials/multiple/delete', 'Ajax\MaterialController@destroyMultipleMaterials')->name("destroyMultipleMaterials.datatable");
+    Route::patch('/materials/multiple/add-material', 'Ajax\MaterialController@addMaterialMultiple')->name("addMaterialMultiple.datatable");
+    Route::patch('/materials/multiple/changeStatus', 'Ajax\MaterialController@changeStatusMultiple')->name("changeStatusMultipleMaterial.datatable");;
+    Route::patch('materials/toggle-active/{material}', 'Ajax\MaterialController@toggleActive');
 //! Dashboard Ajax Materials CRUD
-Route::patch('materials/toggle-status/{material}', 'Ajax\MaterialController@toggleStatus');
-Route::delete('/materials/multiple/course/delete', 'Ajax\MaterialController@destroyMultipleCourse')->name("destroyMultipleCourse.datatable");
-Route::post('/materials/add-course/', 'Ajax\MaterialController@addCourse');
-Route::post('/materials/add-course/multiple', 'Ajax\MaterialController@addCourseMultiple');
-Route::post('/materials/cover/upload', 'Ajax\MaterialController@coverUpload')->name('user.cover.upload');
-Route::post('materials/gallery-upload', 'Ajax\MaterialController@galleryUpload')->name('user.gallery.upload');
-Route::patch('material/images-sort', 'Ajax\MaterialController@gallerySort');
+    Route::patch('materials/toggle-status/{material}', 'Ajax\MaterialController@toggleStatus');
+    Route::delete('/materials/multiple/course/delete', 'Ajax\MaterialController@destroyMultipleCourse')->name("destroyMultipleCourse.datatable");
+    Route::post('/materials/add-course/', 'Ajax\MaterialController@addCourse');
+    Route::post('/materials/add-course/multiple', 'Ajax\MaterialController@addCourseMultiple');
+    Route::post('/materials/cover/upload', 'Ajax\MaterialController@coverUpload')->name('user.cover.upload');
+    Route::post('materials/gallery-upload', 'Ajax\MaterialController@galleryUpload')->name('user.gallery.upload');
+    Route::patch('material/images-sort', 'Ajax\MaterialController@gallerySort');
 // Route::delete( 'material/detach-all-files/{material}', 'Ajax\MaterialController@detachAllFiles');
 //! Dashboard Topics Datatables
-Route::post('topics/topics-datatable', 'Ajax\TopicController@index');
+    Route::post('topics/topics-datatable', 'Ajax\TopicController@index');
 //! Dashboard Ajax Topic CRUD
-Route::patch('topics/update/{topic}', 'Ajax\TopicController@update');
-Route::delete('topics/destroy/{ids}', 'Ajax\TopicController@destroy');
-Route::patch('topics/change/color', 'Ajax\TopicController@changeColor');
+    Route::patch('topics/update/{topic}', 'Ajax\TopicController@update');
+    Route::delete('topics/destroy/{ids}', 'Ajax\TopicController@destroy');
+    Route::patch('topics/change/color', 'Ajax\TopicController@changeColor');
 //! Dashboard File Manager Datatable
-Route::post('/file-manager', 'Ajax\MediaController@fileManagerTable');
+    Route::post('/file-manager', 'Ajax\MediaController@fileManagerTable');
 //! Dashboard Course - Materials Datatable
-Route::post('media/remaining-files', 'Ajax\MediaController@remainingFilesTable');
+    Route::post('media/remaining-files', 'Ajax\MediaController@remainingFilesTable');
 //! Dashboard File Manager
-Route::get('media/images', 'Ajax\MediaController@index');
+    Route::get('media/images', 'Ajax\MediaController@index');
 //! Dashboard Gallery
-Route::post('media/gallery', 'Ajax\MediaController@addToGallery');
-Route::post('media/gallery-remove', 'Ajax\MediaController@removeFromGallery');
+    Route::post('media/gallery', 'Ajax\MediaController@addToGallery');
+    Route::post('media/gallery-remove', 'Ajax\MediaController@removeFromGallery');
 //! Dashboard Global Search
-Route::post("dashboard-search", "Ajax\DashboardController@globalSearch");
+    Route::post("dashboard-search", "Ajax\DashboardController@globalSearch");
 //! Ajax Upload Files
-Route::post('materials/upload-description-images', 'Ajax\MaterialController@uploadDescImages');
-Route::post('media/upload-images', 'Ajax\MediaController@editorImages');
-Route::post('media/files-upload', 'Ajax\MediaController@fileUpload');
-Route::patch('media/cover/replace', 'Ajax\MediaController@coverChange');
-Route::patch('media/cover/not-exist', 'Ajax\MediaController@coverChangeNotExist');
-Route::post('media/add-files', 'Ajax\MediaController@addFiles');
-Route::post('media/remove-files', 'Ajax\MediaController@removeFiles');
+    Route::post('materials/upload-description-images', 'Ajax\MaterialController@uploadDescImages');
+    Route::post('media/upload-images', 'Ajax\MediaController@editorImages');
+    Route::post('media/files-upload', 'Ajax\MediaController@fileUpload');
+    Route::patch('media/cover/replace', 'Ajax\MediaController@coverChange');
+    Route::patch('media/cover/not-exist', 'Ajax\MediaController@coverChangeNotExist');
+    Route::post('media/add-files', 'Ajax\MediaController@addFiles');
+    Route::post('media/remove-files', 'Ajax\MediaController@removeFiles');
 //!======================================================
 //! 			End ajax Routes					|
 //!======================================================
-Auth::routes();
 //!######################################################
 //!					Index  Routes					#
 //!######################################################
-
-
-
+});
 //!######################################################
 //!					middleware				            #
 Route::group(['middleware' => ['auth']], function () {
-    Route::get('/home', 'Index\HomeController@index')->name('home');
-
     //home index
+    Route::get('/home', 'Index\HomeController@index')->name('home');
+    //excehl
+    Route::get("/expot/users", "ExportController@usersAll")->name("export.usersAll");
 //user-profile index
     Route::get('/{user}/profile', 'Index\UserController@index')->name('index.profile');
     Route::patch('/{user}/profile/update', 'Index\UserController@update')->name('index.profile.update');
@@ -196,8 +194,17 @@ Route::group(['middleware' => ['auth']], function () {
     Route::get("/message/{user}", "MessageController@receiver")->name("index.receiver");
     Route::get("/message/info/{user}", "MessageController@info")->name("index.info");
     Route::post("/message/sent", "MessageController@sendMessage")->name("index.sendMessage");
+//! partner indexN
+    Route::get("/user/link", "Index\HomeController@createLink")->name("user.link");
+    Route::post("/user/link/store", "Index\HomeController@createLinkStore")->name("user.linkStore");
+    Route::get("/user/view-link", "Index\HomeController@showLinks")->name("user.showLinks");
+    Route::get('/partner-links', function (Request $request) {
+        if (!$request->hasValidSignature())
+        {
+            abort(401);
+        }
+    })->name('link');
 });
-
 //!######################################################
 //!					middleware				            #
 Route::group(['middleware' => ['auth', "verifyCourse"]], function () {
@@ -212,21 +219,6 @@ Route::group(['middleware' => ['auth', "verifyCourse"]], function () {
 //! Material index  ajax
     Route::patch('/add-watchlist/material', 'Index\MaterialController@watchlistMaterial')->name('index.watchlist.material');
 });
-
-
-//! partner indexN
-    Route::get("/user/link", "Index\HomeController@createLink")->name("user.link");
-    Route::post("/user/link/store", "Index\HomeController@createLinkStore")->name("user.linkStore");
-    Route::get("/user/view-link", "Index\HomeController@showLinks")->name("user.showLinks");
-    Route::get('/partner-links', function (Request $request) {
-        if (!$request->hasValidSignature())
-        {
-            abort(401);
-        }
-    })->name('link');
-
-
-
 
 
 
