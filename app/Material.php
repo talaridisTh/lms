6da1e5505a94cf54f1be0184af56834f3c002a04
Evangelos Bitsilis <e.bitsilis@gmail.com>
2020-10-05 17:19:25 +0300

    chapter priority fix

--- app/Material.php
+++ app/Material.php
@@ -18,7 +18,7 @@ class Material extends Model {
         return $this->belongsToMany(
             Material::class, "material_section",
             "section_id", "material_id"
-        )->withPivot('status', 'priority');
+        )->withPivot('status', 'priority')->orderBy("material_section.priority");
     }
 
     public function courses()
