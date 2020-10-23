bb5296acc016cf6986917f0b58cf676967d919b9
Evangelos Bitsilis <e.bitsilis@gmail.com>
2020-10-23 11:34:54 +0300

    fixed users dates

--- resources/js/dashboard/users/userMain.js
+++ resources/js/dashboard/users/userMain.js
@@ -38,7 +38,21 @@ const tables = $("#scroll-horizontal-datatable").DataTable({
         {data: "email", name: "email", className: "js-link cursor-pointer"},
         {data: 'status', name: 'status', orderable: false},
         // {data: 'created', name: 'created',orderData: [ 10],visible:false},
-        {data: 'created_at', name: 'users.created_at', orderData: [9]},
+        {
+			data: 'created_at',
+			name: 'users.created_at', 
+			orderData: [9],
+			className: "text-center",
+			render: function(data) {
+				let date = new Date(data);
+				let day = date.toLocaleDateString().replace( /[/]/g, "-");
+				let hours = `${date.getHours()}`.padStart(2, "0");
+				let minutes = `${date.getMinutes()}`.padStart(2, "0");
+
+				let time = `${hours}:${minutes}`;
+				return `<p class="mb-0">${day}</p><p class="mb-0">${time}</p>`;
+			}
+		},
         {data: 'courses', name: 'courses', orderable: false, visible: false},
         {data: 'id', name: 'id',visible: false},
     ],
