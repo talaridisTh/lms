b424a65dc04cb5fc3e39cad9c256ad583118705f
thanos <talaridis@gmail.com>
2020-10-29 18:06:32 +0200

    change cover to avatar

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,57 +26,58 @@ class DatabaseSeeder extends Seeder {
     {
 
 //        live
-//        $this->call([
-//            RoleSeeder::class,
-//            UserSeeder::class,
-//        ]);
 
-
-        // local
         $this->call([
-			RoleSeeder::class,
-			TopicSeeder::class,
-			UserSeeder::class,
-			UtilitySeeder::class,
-		]);
+            RoleSeeder::class,
+            UserSeeder::class,
+        ]);
 
-		User::factory()->times(500)->create()
-			->each( function($user) {
-        		$user->roles()->attach( rand( 2, 4 ) );
-			});
 
-		Material::factory()->times(80)->create()
-			->each( function($material) {
-
-				$material->users()->attach( Role::find(2)->users()->select("id")->get()->random()->id);
-
-				if ( $material->type == "Section" ) {
-					for ( $i = 0; $i < 5; $i++ ) {
-						$material->chapters()->attach( rand(1, 80), ["priority" =>  $i + 1]);
-					}
-				}
-			});
-
-		Course::factory()->times(30)->create()
-			->each( function($course) {
-				$course->topics()->attach( Topic::all()->random()->id);
-				for ( $i = 0; $i < 20; $i++) {
-					$course->users()->attach(User::where('id', '!=', 1)->get()->random()->id);
-
-					$course->materials()->attach( rand(1, 80), ["priority" => $i + 1]);
-
-				}
-			});
-
-		Bundle::factory()->times(15)->create()
-			->each( function($bundle) {
-				for( $i = 0; $i < 5; $i++ ) {
-					$bundle->courses()->attach(Course::all()->random()->id);
-				}
-				for ( $i = 0; $i < 20; $i++ ) {
-					$bundle->users()->attach(Role::find(4)->users()->select("id")->get()->random()->id);
-				}
-			});
+//        // local
+//        $this->call([
+//			RoleSeeder::class,
+//			TopicSeeder::class,
+//			UserSeeder::class,
+//			UtilitySeeder::class,
+//		]);
+//
+//		User::factory()->times(500)->create()
+//			->each( function($user) {
+//        		$user->roles()->attach( rand( 2, 4 ) );
+//			});
+//
+//		Material::factory()->times(80)->create()
+//			->each( function($material) {
+//
+//				$material->users()->attach( Role::find(2)->users()->select("id")->get()->random()->id);
+//
+//				if ( $material->type == "Section" ) {
+//					for ( $i = 0; $i < 5; $i++ ) {
+//						$material->chapters()->attach( rand(1, 80), ["priority" =>  $i + 1]);
+//					}
+//				}
+//			});
+//
+//		Course::factory()->times(30)->create()
+//			->each( function($course) {
+//				$course->topics()->attach( Topic::all()->random()->id);
+//				for ( $i = 0; $i < 20; $i++) {
+//					$course->users()->attach(User::where('id', '!=', 1)->get()->random()->id);
+//
+//					$course->materials()->attach( rand(1, 80), ["priority" => $i + 1]);
+//
+//				}
+//			});
+//
+//		Bundle::factory()->times(15)->create()
+//			->each( function($bundle) {
+//				for( $i = 0; $i < 5; $i++ ) {
+//					$bundle->courses()->attach(Course::all()->random()->id);
+//				}
+//				for ( $i = 0; $i < 20; $i++ ) {
+//					$bundle->users()->attach(Role::find(4)->users()->select("id")->get()->random()->id);
+//				}
+//			});
 
 	}
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -22,168 +22,168 @@ class UserSeeder extends Seeder {
     {
 
         //live
-//        $admin = User::create([
-//            'first_name' => "admin",
-//            'last_name' => "admin",
-//            'email' => "admin@gmail.com",
-//            "phone" => "6978565698",
-//            "profil" => "admin profil",
-//            'avatar' => "/images/avatar-placeholder.png",
-//            'slug' => "admin",
-//            'password' => Hash::make('password'),
-//            "password_encrypt" => Crypt::encryptString('password'),
-//            'status' => 1,
-//            'remember_token' => Str::random(10),
-//        ])->assignRole("admin");
+        $admin = User::create([
+            'first_name' => "admin",
+            'last_name' => "admin",
+            'email' => "admin@gmail.com",
+            "phone" => "6978565698",
+            "profil" => "admin profil",
+            'avatar' => "/images/avatar-placeholder.png",
+            'slug' => "admin",
+            'password' => Hash::make('password'),
+            "password_encrypt" => Crypt::encryptString('password'),
+            'status' => 1,
+            'remember_token' => Str::random(10),
+        ])->assignRole("admin");
 
 
     //local
 
-       $admin= User::create([
-            'first_name' => "admin",
-            'last_name' => "admin",
-            'email' => "admin@gmail.com",
-            "phone"=>"6978565698",
-            "profil"=>"admin profil",
-            'avatar' => "/images/avatar-placeholder.png",
-            'slug' => "admin",
-            'password' => Hash::make('password'),
-            "password_encrypt"=>Crypt::encryptString('password'),
-            'status' => 1,
-            'remember_token' => Str::random(10),
-		])->assignRole("admin")
-            ->each( function() {
-              $javascript =  Course::create([
-                        'title' => "Javascript",
-                        "subtitle"=>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dignissim pulvinar metus quis bibendum. Donec fermentum porta consequat. Pellentesque vestibulum magna convallis, consequat neque a.",
-                        "summary"=>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dignissim pulvinar metus quis bibendum. Donec fermentum porta consequat. Pellentesque vestibulum magna convallis, consequat neque a.",
-                        "cover"=>"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRuHnJDLOcdm_0b6N6kNj-1OvO9KhKYgqIy0w&usqp=CAU",
-                        "description"=>'<div class="row"><div class="col-6"><p class="text-wrap"><br><!--StartFragment-->Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dignissim <br>pulvinar metus quis bibendum. Donec fermentum porta consequat. <br>Pellentesque vestibulum magna convallis, consequat neque a.<!--EndFragment--><br></p></div><div class="col-6"><blockquote><p class="text-wrap">Quote...</p><p class="text-wrap"><cite>Author Attribution<br><!--StartFragment-->Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dignissim <br>pulvinar metus quis bibendum. Donec fermentum porta consequat. <br>Pellentesque vestibulum magna convallis, consequat neque a.<!--EndFragment--><br><br></cite></p></blockquote></div></div>',
-                        "user_id"=>2,
-                        "slug"=>"javascript",
-                        "version"=>"Trial",
-						"status"=>1,
-						'fields' => json_encode(["summary" => rand(0,1), "description" => rand(0,1)]),
-                        "publish_at"=>now()
-                    ]
-                );
-                $javascript->users()->sync(User::find(1));
-                $javascript->topics()->sync(Topic::find(1));
-                $javascript->materials()->create([
-                    'title' => "Variables",
-                    'slug' => "Variables",
-                    'subtitle' => "sagittis, quis sagittis ex convallis. Pellentesque suscipit, neque vitae finibus pretium. ",
-                    'summary' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dignissim odio sit amet magna sagittis, quis sagittis ex convallis. Pellentesque suscipit, neque vitae finibus pretium. ",
-                    'cover' => "https://img-a.udemycdn.com/course/750x422/1929394_4932_4.jpg",
-                    'description' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dignissim odio sit amet magna sagittis, quis sagittis ex convallis. Pellentesque suscipit, neque vitae finibus pretium. ",
-                    'content' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dignissim odio sit amet magna sagittis, quis sagittis ex convallis. Pellentesque suscipit, neque vitae finibus pretium. ",
-					'fields' => json_encode(["summary" => rand(0,1), "description" => rand(0,1), "content" => rand(0,1)]),
-					'status' => "1",
-                    'type' => "Lesson",
-
-
-                ],["priority"=>1 ,"status"=>1]);
-
-               $laravel =   Course::create([
-                    'title' => "Laravel",
-                    "subtitle"=>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dignissim pulvinar metus quis bibendum. Donec fermentum porta consequat. Pellentesque vestibulum magna convallis, consequat neque a.",
-                    "summary"=>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dignissim pulvinar metus quis bibendum. Donec fermentum porta consequat. Pellentesque vestibulum magna convallis, consequat neque a.",
-                    "cover"=>"https://cdn.slidesharecdn.com/ss_thumbnails/kickstartmv-techtalk-on-laravel-140526012910-phpapp01-thumbnail-4.jpg?cb=1401068117",
-                    "user_id"=>2,
-                    "slug"=>"laravel",
-                    "version"=>"Trial",
-					"status"=>1,
-					'fields' => json_encode(["summary" => rand(0,1), "description" => rand(0,1)]),
-                    "publish_at"=>now()
-                ]);
-                $laravel->users()->sync(User::find(1));
-                $laravel->topics()->sync(Topic::find(2));
-                $laravel->materials()->create([
-                    'title' => "Controller",
-                    'slug' => "controller",
-                    'subtitle' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dignissim odio sit amet magna sagittis, quis sagittis ex convallis. Pellentesque suscipit, neque vitae finibus pretium. ",
-                    'summary' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dignissim odio sit amet magna sagittis, quis sagittis ex convallis. Pellentesque suscipit, neque vitae finibus pretium. ",
-                    'cover' => "https://miro.medium.com/max/960/1*Vu2ThQPxr72UEOP0LMo4hA.jpeg",
-                    'description' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dignissim odio sit amet magna sagittis, quis sagittis ex convallis. Pellentesque suscipit, neque vitae finibus pretium. ",
-                    'content' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dignissim odio sit amet magna sagittis, quis sagittis ex convallis. Pellentesque suscipit, neque vitae finibus pretium. ",
-					'fields' => json_encode(["summary" => rand(0,1), "description" => rand(0,1), "content" => rand(0,1)]),
-					'status' => "1",
-                    'type' => "Link",
-                    'video_Link' => "43014286",
-
-
-                ],["priority"=>1 ,"status"=>1]);
-
-            });
-
-		User::create([
-            'first_name' => "Υδρόγειος",
-            'last_name' => "Idrogios",
-            'avatar' => "/images/avatar-placeholder.png",
-            'email' => "idrogios@gmail.com",
-            "phone"=>"6978512598",
-            "profil"=>"
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dignissim odio sit amet magna sagittis, quis sagittis ex convallis. Pellentesque suscipit, neque vitae finibus pretium. ",
-            'slug' => "Idrogios",
-            'password' => Hash::make('password'),
-            "password_encrypt"=>Crypt::encryptString('password'),
-            'status' => 1,
-            'remember_token' => Str::random(10),
-        ])->assignRole("instructor");
-        User::create([
-            'first_name' => "instructor",
-            'last_name' => "instructor",
-            'email' => "instructor@gmail.com",
-            "phone"=>"6978562698",
-            "profil"=>"instructor profil",
-            'avatar' => "/images/avatar-placeholder.png",
-            'slug' => "instructor",
-            'password' => Hash::make('password'),
-            "password_encrypt"=>Crypt::encryptString('password'),
-            'status' => 1,
-            'remember_token' => Str::random(10),
-        ])->assignRole("instructor");
-        User::create([
-            'first_name' => "partner",
-            'last_name' => "partner",
-            'email' => "partner@gmail.com",
-            "phone"=>"6978564398",
-            "profil"=>"admin",
-            'avatar' => "/images/avatar-placeholder.png",
-            'slug' => "partner profil",
-            'password' => Hash::make('password'),
-            "password_encrypt"=>Crypt::encryptString('password'),
-            'status' => 1,
-            'remember_token' => Str::random(10),
-        ])->assignRole("partner");
-
-        User::create([
-            'first_name' => "student",
-            'last_name' => "student",
-            'email' => "student@gmail.com",
-            "phone"=>"6978523498",
-            "profil"=>"student profil",
-            'avatar' => "/images/avatar-placeholder.png",
-            'slug' => "student",
-            'password' => Hash::make('password'),
-            "password_encrypt"=>Crypt::encryptString('password'),
-            'status' => 1,
-            'remember_token' => Str::random(10),
-        ])->assignRole("student");
-        User::create([
-            'first_name' => "trial user",
-            'last_name' => "trial user",
-            'email' => "trial-user@gmail.com",
-            "phone"=>"6928523498",
-            "profil"=>"trial user",
-            'avatar' => "/images/avatar-placeholder.png",
-            'slug' => "trial-user",
-            'password' => Hash::make('password'),
-            "password_encrypt"=>Crypt::encryptString('password'),
-            'status' => 1,
-            'remember_token' => Str::random(10),
-        ])->assignRole("trial user");
+//       $admin= User::create([
+//            'first_name' => "admin",
+//            'last_name' => "admin",
+//            'email' => "admin@gmail.com",
+//            "phone"=>"6978565698",
+//            "profil"=>"admin profil",
+//            'avatar' => "/images/avatar-placeholder.png",
+//            'slug' => "admin",
+//            'password' => Hash::make('password'),
+//            "password_encrypt"=>Crypt::encryptString('password'),
+//            'status' => 1,
+//            'remember_token' => Str::random(10),
+//		])->assignRole("admin")
+//            ->each( function() {
+//              $javascript =  Course::create([
+//                        'title' => "Javascript",
+//                        "subtitle"=>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dignissim pulvinar metus quis bibendum. Donec fermentum porta consequat. Pellentesque vestibulum magna convallis, consequat neque a.",
+//                        "summary"=>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dignissim pulvinar metus quis bibendum. Donec fermentum porta consequat. Pellentesque vestibulum magna convallis, consequat neque a.",
+//                        "cover"=>"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRuHnJDLOcdm_0b6N6kNj-1OvO9KhKYgqIy0w&usqp=CAU",
+//                        "description"=>'<div class="row"><div class="col-6"><p class="text-wrap"><br><!--StartFragment-->Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dignissim <br>pulvinar metus quis bibendum. Donec fermentum porta consequat. <br>Pellentesque vestibulum magna convallis, consequat neque a.<!--EndFragment--><br></p></div><div class="col-6"><blockquote><p class="text-wrap">Quote...</p><p class="text-wrap"><cite>Author Attribution<br><!--StartFragment-->Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dignissim <br>pulvinar metus quis bibendum. Donec fermentum porta consequat. <br>Pellentesque vestibulum magna convallis, consequat neque a.<!--EndFragment--><br><br></cite></p></blockquote></div></div>',
+//                        "user_id"=>2,
+//                        "slug"=>"javascript",
+//                        "version"=>"Trial",
+//						"status"=>1,
+//						'fields' => json_encode(["summary" => rand(0,1), "description" => rand(0,1)]),
+//                        "publish_at"=>now()
+//                    ]
+//                );
+//                $javascript->users()->sync(User::find(1));
+//                $javascript->topics()->sync(Topic::find(1));
+//                $javascript->materials()->create([
+//                    'title' => "Variables",
+//                    'slug' => "Variables",
+//                    'subtitle' => "sagittis, quis sagittis ex convallis. Pellentesque suscipit, neque vitae finibus pretium. ",
+//                    'summary' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dignissim odio sit amet magna sagittis, quis sagittis ex convallis. Pellentesque suscipit, neque vitae finibus pretium. ",
+//                    'cover' => "https://img-a.udemycdn.com/course/750x422/1929394_4932_4.jpg",
+//                    'description' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dignissim odio sit amet magna sagittis, quis sagittis ex convallis. Pellentesque suscipit, neque vitae finibus pretium. ",
+//                    'content' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dignissim odio sit amet magna sagittis, quis sagittis ex convallis. Pellentesque suscipit, neque vitae finibus pretium. ",
+//					'fields' => json_encode(["summary" => rand(0,1), "description" => rand(0,1), "content" => rand(0,1)]),
+//					'status' => "1",
+//                    'type' => "Lesson",
+//
+//
+//                ],["priority"=>1 ,"status"=>1]);
+//
+//               $laravel =   Course::create([
+//                    'title' => "Laravel",
+//                    "subtitle"=>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dignissim pulvinar metus quis bibendum. Donec fermentum porta consequat. Pellentesque vestibulum magna convallis, consequat neque a.",
+//                    "summary"=>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dignissim pulvinar metus quis bibendum. Donec fermentum porta consequat. Pellentesque vestibulum magna convallis, consequat neque a.",
+//                    "cover"=>"https://cdn.slidesharecdn.com/ss_thumbnails/kickstartmv-techtalk-on-laravel-140526012910-phpapp01-thumbnail-4.jpg?cb=1401068117",
+//                    "user_id"=>2,
+//                    "slug"=>"laravel",
+//                    "version"=>"Trial",
+//					"status"=>1,
+//					'fields' => json_encode(["summary" => rand(0,1), "description" => rand(0,1)]),
+//                    "publish_at"=>now()
+//                ]);
+//                $laravel->users()->sync(User::find(1));
+//                $laravel->topics()->sync(Topic::find(2));
+//                $laravel->materials()->create([
+//                    'title' => "Controller",
+//                    'slug' => "controller",
+//                    'subtitle' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dignissim odio sit amet magna sagittis, quis sagittis ex convallis. Pellentesque suscipit, neque vitae finibus pretium. ",
+//                    'summary' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dignissim odio sit amet magna sagittis, quis sagittis ex convallis. Pellentesque suscipit, neque vitae finibus pretium. ",
+//                    'cover' => "https://miro.medium.com/max/960/1*Vu2ThQPxr72UEOP0LMo4hA.jpeg",
+//                    'description' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dignissim odio sit amet magna sagittis, quis sagittis ex convallis. Pellentesque suscipit, neque vitae finibus pretium. ",
+//                    'content' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dignissim odio sit amet magna sagittis, quis sagittis ex convallis. Pellentesque suscipit, neque vitae finibus pretium. ",
+//					'fields' => json_encode(["summary" => rand(0,1), "description" => rand(0,1), "content" => rand(0,1)]),
+//					'status' => "1",
+//                    'type' => "Link",
+//                    'video_Link' => "43014286",
+//
+//
+//                ],["priority"=>1 ,"status"=>1]);
+//
+//            });
+//
+//		User::create([
+//            'first_name' => "Υδρόγειος",
+//            'last_name' => "Idrogios",
+//            'avatar' => "/images/avatar-placeholder.png",
+//            'email' => "idrogios@gmail.com",
+//            "phone"=>"6978512598",
+//            "profil"=>"
+//
+//Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dignissim odio sit amet magna sagittis, quis sagittis ex convallis. Pellentesque suscipit, neque vitae finibus pretium. ",
+//            'slug' => "Idrogios",
+//            'password' => Hash::make('password'),
+//            "password_encrypt"=>Crypt::encryptString('password'),
+//            'status' => 1,
+//            'remember_token' => Str::random(10),
+//        ])->assignRole("instructor");
+//        User::create([
+//            'first_name' => "instructor",
+//            'last_name' => "instructor",
+//            'email' => "instructor@gmail.com",
+//            "phone"=>"6978562698",
+//            "profil"=>"instructor profil",
+//            'avatar' => "/images/avatar-placeholder.png",
+//            'slug' => "instructor",
+//            'password' => Hash::make('password'),
+//            "password_encrypt"=>Crypt::encryptString('password'),
+//            'status' => 1,
+//            'remember_token' => Str::random(10),
+//        ])->assignRole("instructor");
+//        User::create([
+//            'first_name' => "partner",
+//            'last_name' => "partner",
+//            'email' => "partner@gmail.com",
+//            "phone"=>"6978564398",
+//            "profil"=>"admin",
+//            'avatar' => "/images/avatar-placeholder.png",
+//            'slug' => "partner profil",
+//            'password' => Hash::make('password'),
+//            "password_encrypt"=>Crypt::encryptString('password'),
+//            'status' => 1,
+//            'remember_token' => Str::random(10),
+//        ])->assignRole("partner");
+//
+//        User::create([
+//            'first_name' => "student",
+//            'last_name' => "student",
+//            'email' => "student@gmail.com",
+//            "phone"=>"6978523498",
+//            "profil"=>"student profil",
+//            'avatar' => "/images/avatar-placeholder.png",
+//            'slug' => "student",
+//            'password' => Hash::make('password'),
+//            "password_encrypt"=>Crypt::encryptString('password'),
+//            'status' => 1,
+//            'remember_token' => Str::random(10),
+//        ])->assignRole("student");
+//        User::create([
+//            'first_name' => "trial user",
+//            'last_name' => "trial user",
+//            'email' => "trial-user@gmail.com",
+//            "phone"=>"6928523498",
+//            "profil"=>"trial user",
+//            'avatar' => "/images/avatar-placeholder.png",
+//            'slug' => "trial-user",
+//            'password' => Hash::make('password'),
+//            "password_encrypt"=>Crypt::encryptString('password'),
+//            'status' => 1,
+//            'remember_token' => Str::random(10),
+//        ])->assignRole("trial user");
 
 
 }
