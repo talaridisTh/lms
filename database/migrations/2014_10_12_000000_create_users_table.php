77e7f9a0c05b7be80f6ccd34715ff7c99d5199fc
thanos <talaridis@gmail.com>
2020-10-29 21:29:32 +0200

    compine table system

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,9 +20,12 @@ class CreateUsersTable extends Migration
             $table->string('name', 50)->nullable(); // na min svisti xriazete gia to message system
             $table->string('last_name', 50);
             $table->string('email')->unique();
+            $table->string('avatar')->default(config('chatify.user_avatar.default'))->nullable();
+            $table->string('messenger_color')->default('#2180f3');
+            $table->boolean('dark_mode')->default(0);
+            $table->boolean('active_status')->default(0);
             $table->bigInteger('phone');
             $table->text('profil')->nullable();
-            $table->string('avatar')->nullable();
             $table->string('facebook_link')->unique()->nullable();
             $table->string('instagram_link')->unique()->nullable();
             $table->string('linkedin_link')->unique()->nullable();
@@ -35,6 +38,7 @@ class CreateUsersTable extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
+
     }
 
     /**
