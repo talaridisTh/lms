acb1eb02c750966044a15f417c22960e9d6e82af
thanos <talaridis@gmail.com>
2020-11-07 12:23:21 +0200

    update

--- app/Material.php
+++ app/Material.php
@@ -94,8 +94,11 @@ class Material extends Model {
     public static function getIcon($value)
     {
         $icons = [
-            "mp3" => "mdi-play-circle-outline",
+            "mp3" => "mdi-music-clef-treble",
+            "ev3" => "mdi-robot-industrial",
             "pdf" => "mdi-file-pdf-outline text-danger",
+            "html" => "mdi-language-html5 text-danger",
+            "odg" => "mdi-file-pdf text-danger",
             "doc" => "mdi-file-document-outline text-teal",
             "odt" => "mdi-file-document-outline text-teal",
             "rtf" => "mdi-file-document-outline text-teal",
@@ -103,7 +106,9 @@ class Material extends Model {
             "ods" => "mdi-file-table-box text-success",
             "pp" => "mdi-file-powerpoint-outline text-orange",
             "odp" => "mdi-file-powerpoint-outline text-orange",
+            "sb3" => "mdi-cat text-orange",
             "zip" => "mdi-folder-zip-outline text-warning",
+            "rar" => "mdi-folder-zip-outline text-warning",
         ];
         foreach ($icons as $type => $icon)
         {
