172e3b2ce03589d063f5bcb88e8745f2476e8b02
Evangelos Bitsilis <e.bitsilis@gmail.com>
2020-11-16 22:12:05 +0200

    Glide change

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Media;
-use App\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Str;
+use League\Flysystem\Adapter\Local;
+use League\Flysystem\Filesystem as FlysystemFilesystem;
+use League\Glide\Server;
+use League\Glide\Signatures\SignatureFactory;
+use League\Glide\Signatures\SignatureException;
+use League\Glide\Urls\UrlBuilderFactory;
 
 class MediaController extends Controller
 {
@@ -22,5 +26,34 @@ class MediaController extends Controller
 		];
 
         return view("admin/media/mediaIndex")->with($data);
-    }
+	}
+
+	public function show(Server $server, Request $request, $path) {
+
+		$temp = explode("/", $path);
+		$image = array_pop($temp);
+		$source = implode("/", $temp);
+		array_shift($temp);
+		$cache = implode("/", $temp);
+
+		try {
+			// Set complicated sign key
+			$signkey = 'The strongest of all warriors are these two, patience and time...';
+
+			// Validate HTTP signature
+			SignatureFactory::create($signkey)->validateRequest($request->getPathInfo(), $_GET);
+
+			$source = new FlysystemFilesystem( new Local($source) );
+	
+			$server->setSource($source);
+			$server->setCachePathPrefix(".cache/$cache");
+
+			$server->outputImage($image, $_GET);
+		
+		} catch (SignatureException $e) {
+			// Handle error
+			abort(400);
+		}
+	}
+
 }
